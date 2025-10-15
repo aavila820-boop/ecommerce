301aecdcdeebd0b8c7d9298cf286dd0f2820581d
aavila820-boop <aavila820@unab.edu.co>
2025-10-14 22:30:10 -0500

    Layouts en laravel

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,1 +1,8 @@
+@extends('layouts.app')
 
+@section('content')
+    <h1>Form - Create Product</h1>
+    <form action="" method="POST">
+
+    </form>
+@endsection
--- resources/views/products/detail.blade.php
+++ resources/views/products/detail.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ecommerce</title>
-</head>
-
-<body>
+@extends('layouts.app')
+@section('content')
     <h1>Product details: {{ $id }}</h1>
-</body>
-
-</html>
+    <h1>Product Category: {{ $category }}</h1>
+@endsection
