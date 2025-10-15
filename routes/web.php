6e64c904661bb0e6c31bbb81419d303ab7d45408
aavila820-boop <aavila820@unab.edu.co>
2025-10-14 21:55:41 -0500

    Rutas Basicas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    return "list products";
+});
+
+Route::get('/products/{id}/{category?}', function ($id, $category = null) {
+    if ($category != null) {
+        return "Detail products: " . $id . ". With category: " . $category;
+    } else {
+        return "Detail products: " . $id;
+    }
+});
