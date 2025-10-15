db0fc489404a16742a9c536d4730858eba7f25b0
aavila820-boop <aavila820@unab.edu.co>
2025-10-15 18:36:56 -0500

    Se configuro el login

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
-
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::prefix("products")->controller(ProductController::class)->group(function () {
     Route::get('/', 'index');
     Route::get('/create', 'create');
     Route::get('/{id}/{category?}', 'details');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [HomeController::class, 'welcome']);
